08b0a295a00eb14b0af974aa0171188d0dbff9d5
posssssss <168770882+posssssss@users.noreply.github.com>
2024-05-18 16:41:19 +0200

    Add files via upload

---
+++ Zad2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz.cistKod
+{
+    //metoda tvornice
+    public class WebElement
+    {
+        string name;
+        public WebElement(string name)
+        {
+            Console.WriteLine($"Found {name}");
+            this.name = name;
+        }
+        public void Click()
+        {
+            Console.WriteLine($"Clicked {name}");
+        }
+    }
+
+    public interface ILoginPage
+    {
+        public WebElement loginButton();
+        public WebElement usernameInput();
+        public WebElement passwordInput();
+    }
+
+    public class ChromeLoginPage : ILoginPage
+    {
+        public WebElement loginButton()
+        {
+            return new WebElement("LoginButton");
+        }
+        public WebElement usernameInput()
+        {
+            return new WebElement("userInput");
+        }
+
+        public WebElement passwordInput()
+        {
+            return new WebElement("passwordElement");
+        }
+    }
+    public class FirefoxLoginPage : ILoginPage
+    {
+        public WebElement loginButton()
+        {
+            return new WebElement("LoginButton");
+        }
+        public WebElement usernameInput()
+        {
+            return new WebElement("userInput");
+        }
+
+        public WebElement passwordInput()
+        {
+            return new WebElement("passwordElement");
+        }
+    }
+
+    public abstract class LoginPageFactory
+    {
+        public abstract ILoginPage CreatePage();
+    }
+    public class ChromeLoginPageFactory:LoginPageFactory
+    {
+        public override ILoginPage CreatePage()
+        {
+            return new ChromeLoginPage();
+        }
+    }
+    public class FirefoxLoginPageFactory : LoginPageFactory
+    {
+        public override ILoginPage CreatePage()
+        {
+            return new FirefoxLoginPage(); 
+        }
+    }
+}
---
+++ zad1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz.cistKod
+{
+    //obrazac je builder
+    public class Mail
+    {
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public string Recipient { get; set; }
+        public string Attachments { get; set; }
+    }
+
+    public interface IMailConstructor
+    {
+        public IMailConstructor AddSubject(string subject);
+        public IMailConstructor AddContent(string content);
+        public IMailConstructor AddRecipient(string recipient);
+        public IMailConstructor AddAttachments(string attachments);
+        public IMailConstructor Reset();
+        public Mail Construct();
+
+    }
+    public class MailConstructor: IMailConstructor
+    {
+        Mail mail;
+        public MailConstructor()
+        {
+            mail = new Mail();
+        }
+        public IMailConstructor AddSubject(string recipient)
+        {
+            mail.Recipient = recipient;
+            return this;
+        }
+        public IMailConstructor AddContent(string content)
+        { 
+            mail.Content = content;
+            return this;
+        }
+        public IMailConstructor AddRecipient(string recipient)
+        {
+            mail.Recipient = recipient;
+            return this;
+        }
+        public IMailConstructor AddAttachments(string attachments)
+        { 
+            mail.Attachments = attachments;
+            return this;
+        }
+        public IMailConstructor Reset()
+        {
+            mail = new Mail();
+            return this;
+        }
+        public Mail Construct()
+        {
+            return mail;
+        }
+    }
+
+    public class SMTP
+    {
+        IMailConstructor mailConstructor;
+        public SMTP(IMailConstructor mailConstructor)
+        {
+            this.mailConstructor = mailConstructor;
+        }
+
+        public void SendNoReplyMail()
+        {
+            IMailConstructor mailConstructor;
+
+            mailConstructor.AddSubject("No reply");
+            mailConstructor.AddContent("Hello world");
+            mailConstructor.Construct();
+            //Sending logic here
+        }
+    }
+   
+}
